f561a9ebd5bc02be1442e146aa81f7f73ed157aa
Silas Coutinho <silasmakou@gmail.com>
2021-05-23 23:09:20 -0300

    definir aleatoriedade da comida e tamanho da snake

--- js/script.js
+++ js/script.js
@@ -45,7 +45,6 @@ function iniciarJogo() {
     if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     
-    
     criarBG();
     criarSnake();
     drawFood();
@@ -58,7 +57,12 @@ function iniciarJogo() {
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop(); //retira o último elemento do array
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop(); //retira o último elemento do array
+    }
+    else {food.x = Math.floor(Math.random() * 15 + 1) * box;
+          food.y = Math.floor(Math.random() * 15 + 1) * box;
+         }    
     
     let newHead = {
         x: snakeX,
